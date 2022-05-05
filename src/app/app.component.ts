2addc293a683bc6285593469243f87c750a5d885
Kovlin <romainlink@hotmail.com>
2022-05-05 18:07:06 +0200

    basics

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
+})
+export class AppComponent implements OnInit {
+  pokemonList = [ 'Bulbizarre', 'Salaméche', 'Carapuce'];
+
+  ngOnInit(): void {
+	  console.table(this.pokemonList)
+  }
+}
